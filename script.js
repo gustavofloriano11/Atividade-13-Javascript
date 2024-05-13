fc601d3195ba8d65f6e2f0d28f1cb47c75f2212d
Gustavo Floriano <160735371+gustavofloriano11@users.noreply.github.com>
2024-05-13 09:08:23 -0300

    feito os exercicios da lista 13 de javascript
    arquivos em html, css e js enviados, com o javascript finalizado e podendo ser executado!

---
+++ script.js
@@ -0,0 +1,57 @@
+function exercicio1(){
+    let numero1 = 0;
+    let numero2 = 0;
+    let diferenca = 0;
+    let equacao = 0;
+    numero1 = window.prompt("Digite um número: ")
+    numero2 = window.prompt("Digite outro número: ")
+    diferenca = numero1-numero2
+    equacao = 2*numero1 + 3*numero2
+    window.alert(`A diferença dos números é: ${diferenca}`)
+    window.alert(`O dobro de ${numero1} somado com triplo de ${numero2}, é: ${equacao}`)
+    window.alert(`O dobro de cada uma variável, é: ` + 2*numero1 + ` e ` + 2*numero2)
+    if (numero1>numero2){
+        console.log(numero1 + `, ` + numero2)
+    } else {
+        console.log(numero2 + `, ` + numero1)
+    }
+};
+
+function exercicio2(){
+    let nomeUser = window.prompt("Digite seu nome: ")
+    let salario = window.prompt("Digite seu salário: ")
+    let taxaDesconto = 0.08
+    let INSS = taxaDesconto*salario
+    console.log(nomeUser)
+    console.log(`Seu salário é: R$${salario}`)
+    console.log(`Seu INSS é: R$` + INSS)
+    console.log(`Seu salário líquido, é: R$` + (salario-INSS))
+};
+
+function exercicio3(){
+    let nomeUser = window.prompt("Digite seu nome: ")
+    let salario = window.prompt("Digite seu salário: ")
+    console.log(nomeUser)
+    if (salario <= 1000){
+        let taxaDesconto = 0.08;
+        console.log(`Seu salário é: R$${salario}`)
+        console.log(`Seu desconto é de: ${taxaDesconto}`)
+        let INSS = taxaDesconto*salario
+        console.log(`Seu INSS é: R$` + INSS)
+        console.log(`Seu salário líquido, é: R$` + (salario-INSS))
+    } else if (salario >= 1000.01 && salario <= 1500){
+        taxaDesconto = 0.085
+        console.log(`Seu salário é: R$${salario}`)
+        console.log(`Seu desconto é de: ${taxaDesconto}`)
+        let INSS = taxaDesconto*salario
+        console.log(`Seu INSS é: R$` + INSS)
+        console.log(`Seu salário líquido, é: R$` + (salario-INSS))
+    } else {
+        taxaDesconto = 0.09
+        console.log(`Seu salário é: R$${salario}`)
+        console.log(`Seu desconto é de: ${taxaDesconto}`)
+        let INSS = taxaDesconto*salario
+        console.log(`Seu INSS é: R$` + INSS)
+        console.log(`Seu salário líquido, é: R$` + (salario-INSS))
+    }
+};
